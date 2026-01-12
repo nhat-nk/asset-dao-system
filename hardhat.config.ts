1cfc451b3b2c8e101161618d6bcca5a5a986828f
NhatNK <contact.nhat.nguyenkhac@gmail.com>
2026-01-12 11:34:45 +0700

    Update config and package

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,14 +1,14 @@
 import type { HardhatUserConfig } from "hardhat/config";
+
 import "@nomicfoundation/hardhat-toolbox-viem";
 import "@nomicfoundation/hardhat-node-test-runner";
-import "@nomicfoundation/hardhat-ignition-viem";
-import "@nomicfoundation/hardhat-viem";
+// import "@nomicfoundation/hardhat-ignition-viem";
+// import "@nomicfoundation/hardhat-viem";
 import "dotenv/config";
 
 const config: HardhatUserConfig = {
     solidity: "0.8.28",
     networks: {
-
         localhost: {
             url: "http://127.0.0.1:8545"
         }
