3bb21a3a17dfd1bc11763d420edfdd64c0694b51
NhatNK <contact.nhat.nguyenkhac@gmail.com>
2026-01-12 14:02:31 +0700

    Update hardhat network config

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -10,6 +10,7 @@ const config: HardhatUserConfig = {
     solidity: "0.8.28",
     networks: {
         localhost: {
+            type: "http",
             url: "http://127.0.0.1:8545"
         }
     },
